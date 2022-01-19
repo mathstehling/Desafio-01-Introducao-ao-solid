5bb4d21f8024312416232f48b101564b59911049
Matheus Stehling <matheusfrancostehling@hotmail.com>
2022-01-19 17:31:42 -0300

    Desafio-01-Completo

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,21 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userAdmin = this.usersRepository.findById(user_id);
+
+    if (!userAdmin) {
+      throw new Error("User not exists");
+    }
+
+    if (userAdmin.admin === false) {
+      throw new Error("User is not a admin");
+    }
+
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,10 +3,20 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+
+      const user = this.showUserProfileUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.status(201).json(user);
+    } catch (err) {
+      return response.status(404).json({ error: err });
+    }
   }
 }
 
